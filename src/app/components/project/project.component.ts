253b16ecd08aa34c230ea21fd9b5784639fefc0c
elangelaar <ellenlangelaar@gmail.com>
2017-07-21 14:34:54 +0200

    Added the basic for ProjectComponent

---
+++ src/app/components/project/project.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {Project} from "../../models/project.model";
+
+@Component({
+    templateUrl: './tracks.component.html',
+    styleUrls: ['./tracks.component.scss']
+})
+export class ProjectComponent {
+    project: Project;
+
+    constructor() {
+        this.project = new Project();
+    }
+
+    AddTrack() {
+        this.project.AddTrack();
+    }
+}
