64c4497e67541d398f1be89cbb856e63bb2f8ed3
elangelaar <ellenlangelaar@gmail.com>
2017-07-21 14:23:56 +0200

    Added the project model

---
+++ src/app/models/project.model.ts
@@ -0,0 +1,15 @@
+import { Track } from './track.model';
+
+export class Project {
+    name: string;
+    tracks: Array<Track>;
+
+    AddTrack() {
+        this.tracks.push(new Track(this.GetNextId()));
+    }
+
+    GetNextId(): number {
+        const nextId = this.tracks.slice(-1)[0].id + 1;
+        return nextId ? nextId : 1;
+    }
+}
